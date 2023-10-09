4f72f7869c00cb23e7ea701b90aede0eb1e016db
albinramos <dramosh79@gmail.com>
2023-10-09 13:55:36 +0200

    bichomon

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
 console.log(document.title);
+
+document.getElementById("gen-1").innerHTML="Generasión 1 Pokimon";
+   
+
+document.querySelector('.infocard-list.infocard-list-pkmn-lg').style.backgroundColor = '#CCCCCC';
+
+
+let URLactual = window.location;
+console.log(URLactual);
+
+let URLdominio = window.location.href;
+console.log(URLdominio);
+console.log(location.hostname);
+//te devuelve un nodelist
+let imagenes = document.querySelectorAll("img");
+console.log(imagenes);
+//te devuelve un HTML collection
+let imgs = document.getElementsByTagName("img");
+console.log(imgs);
+
+let todasLasImagenes = document.getElementsByTagName('img');
+for (let i = 0; i < todasLasImagenes.length; i++) {
+    todasLasImagenes[i].src = 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif';
+}
+
+let infocard = document.querySelectorAll('.infocard-lg-data.text-muted');
+for(let i = 0; i < infocard.length; i++){
+    if(infocard[i].innerHTML.includes("itype fying")) {
+        infocard[i].style.backgroundColor="blue";
+    };
+
+}
